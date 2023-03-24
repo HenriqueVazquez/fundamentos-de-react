7958ef8a02994c73d06c6fe5fff96e881a7c72d9
Henrique Vazquez <henrique_pm_vazquez@hotmail.com>
2023-03-24 08:19:53 -0300

    Imutabilidade

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -42,7 +42,7 @@ export function Home() {
         students.map(
           (
             student =>
-              <Card name={student.name} time={student.time} />
+              <Card key={student.time} name={student.name} time={student.time} />
           ))
 
       }
