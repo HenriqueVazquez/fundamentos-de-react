4e42f4df5288000a0436367efb8c8b5d84d45a16
Henrique Vazquez <henrique_pm_vazquez@hotmail.com>
2023-03-24 07:54:56 -0300

    Estado

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,12 +1,20 @@
+import React, { useState } from 'react';
 import './styles.css';
 
 import { Card } from '../../components/Card';
 
 export function Home() {
+  const [studentName, setStudentName] = useState();
+
+
   return (
     <div className='container'>
-      <h1>Lista de presença</h1>
-      <input type="text" placeholder="Digite o nome ..." />
+      <h1>Lista de presença: {studentName}</h1>
+      <input
+        type="text"
+        placeholder="Digite o nome ..."
+        onChange={e => setStudentName(e.target.value)}
+      />
       <button type="button">Adicionar</button>
 
       <Card name="Henrique" time="07:37:47" />
